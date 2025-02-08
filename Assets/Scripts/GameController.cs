9c4f70b5caa894ae7a7bba8b00d9a897e1d798c2
CodeNinjasCoquitlam <coquitlamninja@gmail.com>
2025-02-08 12:05:42 -0800

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        [Header("Platform Object")]
+        public GameObject pltfrm;
+        float pos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
